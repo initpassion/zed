61fe6632c0f98698c986e87655f434ee3a23ef12
涂圣红 <tushenghong01@corp.netease.com>
2018-12-05 17:25:45 +0800

    def basebean

---
+++ initpassion-base/src/main/java/com/initpassion/base/bean/BaseBean.java
@@ -0,0 +1,19 @@
+/*
+ * @(#) BaseBean.java 2018-12-05
+ *
+ * Copyright 2018 NetEase.com, Inc. All rights reserved.
+ */
+
+package com.initpassion.base.bean;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+/**
+ * @author tushenghong01
+ * @version 2018-12-05
+ */
+@Data
+public class BaseBean implements Serializable {
+}
