e1bf995c9a47719ae111bc4cc56899ec6051cc7f
涂圣红 <tushenghong01@corp.netease.com>
2018-12-05 12:36:48 +0800

    init blog module

---
+++ initpassion-blog/src/main/java/com/initpassion/blog/InitpassionBlogApplication.java
@@ -0,0 +1,12 @@
+package com.initpassion.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitpassionBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitpassionBlogApplication.class, args);
+    }
+}
