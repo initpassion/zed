223b2d4e4c794ef14ab12948718d829aa7571fb5
涂圣红 <tushenghong01@corp.netease.com>
2018-12-05 17:18:42 +0800

    rename mq to kafka

--- initpassion-kafka/src/main/java/com/initpassion/kafka/InitpassionKafkaApplication.java
+++ initpassion-kafka/src/main/java/com/initpassion/kafka/InitpassionKafkaApplication.java
@@ -1,12 +1,12 @@
-package com.initpassion.mq;
+package com.initpassion.kafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InitpassionMqApplication {
+public class InitpassionKafkaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(InitpassionMqApplication.class, args);
+        SpringApplication.run(InitpassionKafkaApplication.class, args);
     }
 }
--- initpassion-kafka/src/test/java/com/initpassion/kafka/InitpassionKafkaApplicationTests.java
+++ initpassion-kafka/src/test/java/com/initpassion/kafka/InitpassionKafkaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.initpassion.mq;
+package com.initpassion.kafka;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class InitpassionMqApplicationTests {
+public class InitpassionKafkaApplicationTests {
 
     @Test
     public void contextLoads() {
