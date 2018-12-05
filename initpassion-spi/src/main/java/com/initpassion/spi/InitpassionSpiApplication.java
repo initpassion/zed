4a39a7200e51630f7a20bacfaefa5041f0447c5d
涂圣红 <tushenghong01@corp.netease.com>
2018-12-05 10:50:35 +0800

    init something

---
+++ initpassion-spi/src/main/java/com/initpassion/spi/InitpassionSpiApplication.java
@@ -0,0 +1,12 @@
+package com.initpassion.spi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitpassionSpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitpassionSpiApplication.class, args);
+	}
+}
