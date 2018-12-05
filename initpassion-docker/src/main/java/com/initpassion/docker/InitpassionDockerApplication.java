6945aac85f0e9a002efe8e52152150df1ec9d0b4
涂圣红 <tushenghong01@corp.netease.com>
2018-12-05 12:32:40 +0800

    init docker module

---
+++ initpassion-docker/src/main/java/com/initpassion/docker/InitpassionDockerApplication.java
@@ -0,0 +1,12 @@
+package com.initpassion.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitpassionDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitpassionDockerApplication.class, args);
+    }
+}
