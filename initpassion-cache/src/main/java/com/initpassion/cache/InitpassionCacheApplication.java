88e5ab68c958aef4a2d325e76f48ecfb90e7c07a
涂圣红 <tushenghong01@corp.netease.com>
2019-05-14 14:57:07 +0800

    init cache module

---
+++ initpassion-cache/src/main/java/com/initpassion/cache/InitpassionCacheApplication.java
@@ -0,0 +1,13 @@
+package com.initpassion.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitpassionCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitpassionCacheApplication.class, args);
+    }
+
+}
