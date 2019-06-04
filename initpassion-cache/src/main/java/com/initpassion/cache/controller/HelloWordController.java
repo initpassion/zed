f6bd1f73ec1288bb8de5956b82a7266a72fb7a39
涂圣红 <tushenghong01@corp.netease.com>
2019-06-04 15:05:52 +0800

    init hello

---
+++ initpassion-cache/src/main/java/com/initpassion/cache/controller/HelloWordController.java
@@ -0,0 +1,23 @@
+/*
+ * @(#) HelloWordController.java 2019-06-04
+ *
+ * Copyright 2019 NetEase.com, Inc. All rights reserved.
+ */
+
+package com.initpassion.cache.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tushenghong01
+ * @version 2019-06-04
+ */
+@RestController
+public class HelloWordController {
+
+    @GetMapping(value = "hello")
+    public String hello() {
+        return "initpassion";
+    }
+}
